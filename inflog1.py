324d018cd6b1ed24e635cfafae938367945c0873
vinicius-araujo777 <vinicius.araujo60@aluno.ce.gov.br>
2025-08-05 20:39:01 -0300

    Add files via upload
    Commit da atividade

---
+++ inflog1.py
@@ -0,0 +1,60 @@
+#Questão 1
+#for i in range(1,11):
+#	print(i)
+
+
+#Questão 2
+#n = 10
+#while n >=1:
+#	print(n)
+#	n -=1
+
+
+#Questão 3
+#n = 5
+#nc = input("Digite um nome: ")
+#while n >= 1:
+#	print (nc)
+#	n -= 1
+
+
+#Questão 4
+#for i in range(2,21 ,2):
+#	print(i)
+
+
+#Questão 5
+#nc = input("digite a senha: ")
+#while nc != "1234" :
+#	nc = input("digite a senha: ")
+
+
+#Questão 6
+#n = 1
+#s = 0
+#while n <=5:
+#	s += int(input("digite um numero: "))
+#	n +=1
+#print(f"a soma deles é: {s}")
+
+
+#Questão 7 (pesquisada)
+#for i in range(1, 11):
+#	if i %2  != 0:
+#		continue
+#	print(i)
+
+
+#Questão 8 
+#n = 0
+#nc = int(input("digite um numero: "))
+#while nc != 0:
+#	n += 1
+#	nc = int(input("digite um numero: "))
+#print(n,"vezes ate chegar ao digito 0 ")
+
+
+#Questão 9 (pesquisada)
+#n = int(input("digite um numero"))
+#for i in range(1,11):
+#	print(n, "x", i, "=", n*i)
